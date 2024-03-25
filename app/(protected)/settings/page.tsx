d03c8def96a7bc224982ea6559d408543f1d9f35
zntb <zenetibi@yahoo.com>
2024-03-25 16:25:37 +0200

    Add SessionProvider, move signOut to settings page

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,20 +1,20 @@
-import { auth } from '@/auth';
-import { signOut } from '@/auth';
+'use client';
 
-const SettingsPage = async () => {
-  const session = await auth();
+import { useSession, signOut } from 'next-auth/react';
+
+const SettingsPage = () => {
+  const session = useSession();
+
+  const onClick = () => {
+    signOut();
+  };
 
   return (
     <div>
       {JSON.stringify(session)}
-      <form
-        action={async () => {
-          'use server';
-          await signOut();
-        }}
-      >
-        <button type="submit">Logout</button>
-      </form>
+      <button onClick={onClick} type="submit">
+        Logout
+      </button>
     </div>
   );
 };
